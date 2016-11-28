5fff6ab618524262d5d443be42963cd90ad520ab
tomwi846 <tomwi846@student.liu.se>
2016-11-28 10:36:31 +0100

    Add files via upload
    Character files

---
+++ Character filer/Character.cpp
@@ -0,0 +1,48 @@
+#include "Character.h"
+
+
+
+Character::Character()
+{
+}
+
+
+Character::~Character() 
+{
+
+}
+
+float Character::get_x_pos() 
+{
+	return x_pos;
+}
+
+float Character::get_y_pos() 
+{
+	return y_pos;
+}
+
+void Character::move_x(float x_speed) 
+{
+	set_x_pos(x_speed);
+}
+
+void Character::move_y(float y_speed)
+{
+	set_y_pos(y_speed);
+}
+
+void Character::set_x_pos(float new_pos)
+{
+	x_pos += new_pos;
+}
+
+void Character::set_y_pos(float new_pos)
+{
+	y_pos += new_pos;
+}
+
+void Character::die()
+{
+	
+}
---
+++ Character filer/Enemy/Enemy_3.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Character.h"
+class Enemy_3 :
+	public Character
+{
+public:
+	Enemy_3();
+	~Enemy_3();
+};
+
