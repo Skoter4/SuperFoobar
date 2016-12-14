d9043fa43f2b0c9121a4f534a9d0c93bcdd4ece5
ludda897 <luddan95@gmail.com>
2016-12-14 16:41:03 +0100

    Kommentarer på lite skaer
    hej

--- SuperFoobar/Character filer/Enemy/Enemy_1.cpp
+++ SuperFoobar/Character filer/Enemy/Enemy_1.cpp
@@ -1,3 +1,15 @@
+/*
+* FILENAME:     Enemy_1.cpp
+* PROGRAMMERS:	 Ludvig Danielsson 951221-7937 MED3
+*               Alexander Eriksson 950405-9552 MED3
+*               Martin Allander 951218-6215 Y2b
+*               Tobias Nilsson 950103-6736 MED3
+*               Tomas Widfeldt 940121-2015 MED3
+*
+* DATE:         2016-12-14
+*/
+
+
 #pragma once
 #include "Enemy_1.h"
 #include "../Projectile.h"
@@ -5,15 +17,14 @@
 
 Enemy_1::Enemy_1(int x, int y, int width, int height)
 	: Character(x, y, width, height) 
-	//my_projectile{ std::shared_ptr <Character>{projectile_ptr} }
 {
 }
 
 
 
 std::shared_ptr<Character> Enemy_1::fire_projectile()
-
 {
+	//Runs the Make_track function make_projectile, which creates a Projectile-object
 	return  make_projectile(get_x_pos(), get_y_pos());
 }
 
@@ -30,8 +41,10 @@ void Enemy_1::interact_with(std::shared_ptr<Map_object> map_object)
 	{
 		if (map_object->get_height() > 50)
 		{
+			// If Foobar is huge (have interacted with a mushroom)
 			if ((map_object->get_old_y() + 50 < this->get_y_pos()))
 			{
+				// If Foobar is above the enemy_1
 				this->flip_dead();
 
 			}
--- SuperFoobar/Collision_sfml.h
+++ SuperFoobar/Collision_sfml.h
@@ -1,8 +1,39 @@
+/*
+* FILENAME:     Collision_sfml.h
+* PROGRAMMERS:	 Ludvig Danielsson 951221-7937 MED3
+*               Alexander Eriksson 950405-9552 MED3
+*               Martin Allander 951218-6215 Y2b
+*               Tobias Nilsson 950103-6736 MED3
+*               Tomas Widfeldt 940121-2015 MED3
+*
+* DATE:         2016-12-14
+*
+* DESCRIPTION:
+*
+* A collision handler which controlls the collision between two objects, for example between Foobar and the floor.
+*
+*/
+
 #pragma once
 #include "Map_object.h"
 #include "Character filer\Character.h"
 #include <string>
 #include <memory>
 
+/*
+*	FUNCTION: void block_collision (std::shared_ptr<Character> character_object, std::shared_ptr<Map_object> block_object)
+*	This function controlls the collision between a character_obejct and a block_object.
+*	If a collision occurs it decides from which direction the collsion happen, and then it
+*	changes the colliding objects coordinates/position accordingly.
+*/
+
 void block_collision(std::shared_ptr<Character> character_object, std::shared_ptr<Map_object> block_object);
+
+
+/*
+*	FUNCTION: bool point_in_rect(int x, int y, std::shared_ptr<Map_object> character_object)
+*	This function will return true if the inserted coordinates overlaps with the input object.
+*	It compares the given coordinates with the objects corner coordinates.
+*/
+
 bool point_in_rect(int x, int y, std::shared_ptr<Map_object> character_object);
